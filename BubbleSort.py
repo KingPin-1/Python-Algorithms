feca6c4cc1226b7507909fa62054cea0200c2dea
KING-PIN <mohdathar1991@gmail.com>
2022-06-30 21:44:40 +0530

    completed

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,7 +1,12 @@
 
 def bubbleSort(arr):
+    n = len(arr)
     for i in range(n):
         for j in range(n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j] , arr[j+1] = arr[j+1] , arr[j]
-                
+
+temp = [12,9,14,7,3,1,5,6,8]
+print(temp)
+bubbleSort(temp)
+print(temp)
